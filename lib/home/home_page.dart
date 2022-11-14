7360a222125d9cbc29a2464e7fabae185986c078
Daria Pichlak <dariapichlak.m@gmail.com>
2022-11-14 10:01:18 +0100

    Add BottomNavigationBar

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 1;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hello!')),
+    return Scaffold(
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text('Calendar and Weather'),
+          );
+        }
+        if (currentIndex == 2) {
+          return const Center(
+            child: Text('Notes'),
+          );
+        }
+        return const Center(child: Text('Hello!'));
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note_add),
+            label: 'Plants',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notes),
+            label: 'Notes',
+          ),
+        ],
+      ),
     );
   }
 }
