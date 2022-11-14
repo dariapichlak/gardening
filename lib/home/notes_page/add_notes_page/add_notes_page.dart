ce8a7907432c1d7cd0015fe09b4af602f35cfd90
Daria Pichlak <dariapichlak.m@gmail.com>
2022-11-14 12:37:45 +0100

    Add Textfield to AddNotesPage

---
+++ lib/home/notes_page/add_notes_page/add_notes_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AddNotesPage extends StatefulWidget {
+  const AddNotesPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddNotesPage> createState() => _AddNotesPageState();
+}
+
+var titleNote = '';
+var descriptionNote = '';
+
+class _AddNotesPageState extends State<AddNotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: ListView(
+          children: [
+            TextField(
+              decoration: const InputDecoration(hintText: 'Title'),
+              onChanged: ((newValue) {
+                setState(() {
+                  titleNote = newValue;
+                });
+              }),
+            ),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Description'),
+              onChanged: ((newValue) {
+                setState(() {
+                  descriptionNote = newValue;
+                });
+              }),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.save),
+        label: const Text('Save'),
+        onPressed: () {
+          FirebaseFirestore.instance.collection('notes').add({
+            'descriptionNote': descriptionNote,
+            'titleNote': titleNote,
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/home/notes_page/notes_page_content.dart
+++ lib/home/notes_page/notes_page_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gardening/home/notes_page/add_notes_page/add_notes_page.dart';
 
 class NotesPageContent extends StatelessWidget {
   const NotesPageContent({
@@ -8,26 +9,41 @@ class NotesPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection('notes').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Error'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Loading'));
-          }
-          final documents = snapshot.data!.docs;
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Text(document['note']),
-                ),
-              ]
-            ],
-          );
-        });
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(child: Text('Error'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: Text('Loading'));
+            }
+            final documents = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: ListView(
+                children: [
+                  for (final document in documents) ...[
+                    Text(
+                      document['titleNote'],
+                    ),
+                    Text(
+                      document['descriptionNote'],
+                    ),
+                  ]
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.create),
+        label: const Text('Add Note'),
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => const AddNotesPage()));
+        },
+      ),
+    );
   }
 }
